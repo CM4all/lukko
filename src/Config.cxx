2a525099f107455780bd43926c1d679a5a32b232
Max Kellermann <max.kellermann@ionos.com>
2023-10-22 09:06:34 +0200

    Config: create systemd scope

--- src/Config.cxx
+++ src/Config.cxx
@@ -25,6 +25,9 @@ Config::Config()
 
 	// TODO implement SpawnConfig properly
 	spawn.allow_any_uid_gid = true;
+
+	spawn.systemd_scope = "cm4all-lukko-spawn.scope";
+	spawn.systemd_scope_description = "The Lukko child process spawner";
 }
 
 void
