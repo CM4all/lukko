c7492c4fcabc187a16db65188c73eccd5261be46
Max Kellermann <max.kellermann@ionos.com>
2023-11-12 12:46:59 +0100

    openssl/SerializeEVP: remove special case for EVP_PKEY_EC
    This code that has been added by commit 76a81410199d346 ("check
    base_id, prefer EC-specific code") has been obsoleted by
    c64e4a08315a2314a0494500c3 ("use EVP_PKEY (OpenSSL 3)"); by switching
    to the OpenSSL 3 API, the PKEY is no longer in "legacy" mode and thus
    we aren't any longer affected by OpenSSL bug
    https://github.com/openssl/openssl/pull/22490

--- src/openssl/SerializeEVP.cxx
+++ src/openssl/SerializeEVP.cxx
@@ -3,17 +3,10 @@
 // author: Max Kellermann <mk@cm4all.com>
 
 #include "SerializeEVP.hxx"
-#include "SerializeEC.hxx"
-#include "BN.hxx"
-#include "EC.hxx"
 #include "ssh/Serializer.hxx"
 #include "lib/openssl/Error.hxx"
-#include "lib/openssl/EvpParam.hxx"
-#include "lib/openssl/UniqueEC.hxx"
 
 #include <openssl/core_names.h>
-
-#include <memory>
 
 static void
 SerializeOctetStringParam(SSH::Serializer &s,
@@ -33,43 +26,8 @@ SerializeOctetStringParam(SSH::Serializer &s,
 	s.CommitWriteN(size);
 }
 
-static UniqueEC_GROUP
-GetCurveGroup(const EVP_PKEY &key)
-{
-	const auto group_name = GetStringParam(key, OSSL_PKEY_PARAM_GROUP_NAME);
-
-	const int group_nid = OBJ_sn2nid(group_name.get());
-	if (group_nid == NID_undef)
-		throw SslError{};
-
-	UniqueEC_GROUP group{EC_GROUP_new_by_curve_name(group_nid)};
-	if (group == nullptr)
-		throw SslError{};
-
-	return group;
-}
-
-static void
-SerializePublicKeyEC(SSH::Serializer &s, const EVP_PKEY &key)
-{
-	const auto ec_group = GetCurveGroup(key);
-
-	const auto priv_key = GetBNParam<true>(key, OSSL_PKEY_PARAM_PRIV_KEY);
-	const auto pub_key = EC_POINT_mul(*ec_group, priv_key.get(),
-					  nullptr, nullptr);
-
-	Serialize(s, *pub_key, *ec_group);
-}
-
 void
 SerializePublicKey(SSH::Serializer &s, const EVP_PKEY &key)
 {
-	switch (EVP_PKEY_get_base_id(&key)) {
-	case EVP_PKEY_EC:
-		SerializePublicKeyEC(s, key);
-		break;
-
-	default:
-		SerializeOctetStringParam(s, key, OSSL_PKEY_PARAM_PUB_KEY);
-	}
+	SerializeOctetStringParam(s, key, OSSL_PKEY_PARAM_PUB_KEY);
 }
