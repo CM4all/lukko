56425dd3563a6c8670eab0379796b279dc6287b9
Max Kellermann <max.kellermann@ionos.com>
2024-01-15 15:59:51 +0100

    net/control/Protocol: add CONTROL_PORT

--- src/Config.cxx
+++ src/Config.cxx
@@ -7,6 +7,7 @@
 #include "spawn/ConfigParser.hxx"
 #include "net/IPv6Address.hxx"
 #include "net/Parser.hxx"
+#include "net/control/Protocol.hxx"
 #include "io/config/FileLineParser.hxx"
 #include "io/config/ConfigParser.hxx"
 #include "util/StringAPI.hxx"
@@ -176,7 +177,7 @@ LukkoConfigParser::Control::ParseLine(FileLineParser &line)
 
 	if (StringIsEqual(word, "bind")) {
 		config.bind_address = ParseSocketAddress(line.ExpectValueAndEnd(),
-							 5478, true);
+							 BengProxy::CONTROL_PORT, true);
 	} else if (StringIsEqual(word, "multicast_group")) {
 		config.multicast_group = ParseSocketAddress(line.ExpectValueAndEnd(),
 							    0, false);
