7507b9d787894105202bcc4db2bed20e17aaefb3
Max Kellermann <max.kellermann@ionos.com>
2025-08-14 14:58:02 +0200

    net/control/Protocol: add command DISABLE_URING

--- src/Control.cxx
+++ src/Control.cxx
@@ -68,6 +68,7 @@ Instance::OnControlPacket([[maybe_unused]] BengControl::Server &control_server,
 	case Command::DISABLE_QUEUE:
 	case Command::RELOAD_STATE:
 	case Command::DISCONNECT_DATABASE:
+	case Command::DISABLE_URING:
 		break;
 
 	case Command::TERMINATE_CHILDREN:
