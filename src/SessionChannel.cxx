CM4all/lukko

commit 02019312ce47f66a9eec9c3748deb760903fa4eb

Connection: move code to PrepareChildProcess(ChildOptions)

src/SessionChannel.cxx
@@ -386,7 +386,7 @@ SessionChannel::ExecRsync(const char *cmd, const ExecuteOptions &execute_options
 	FdHolder close_fds;
 	PreparedChildProcess p;
 
-	c.PrepareChildProcess(p, close_fds, execute_options);
+	c.PrepareChildProcess(p, close_fds, execute_options.child_options);
 	PreparePipes(p, close_fds);
 	PrepareHome(alloc, p);
 
@@ -499,7 +499,7 @@ SessionChannel::PrepareSftpServer(AllocatorPtr alloc,
 		/* sftp-server wants to know its own username */
 		p.SetEnv("USER", c.GetUsername());
 
-		c.PrepareChildProcess(p, close_fds, *execute_options);
+		c.PrepareChildProcess(p, close_fds, execute_options->child_options);
 		PreparePipes(p, close_fds);
 		PrepareHome(alloc, p);
 

src/Connection.cxx
@@ -347,10 +347,9 @@ Connection::GetExecuteOptions(SSH::Service service) const noexcept
 void
 Connection::PrepareChildProcess(PreparedChildProcess &p,
 				FdHolder &close_fds,
-				const ExecuteOptions &options) noexcept
+				const ChildOptions &options) noexcept
 {
-	options.child_options.CopyTo(p, close_fds);
-	p.exec_path = options.execute;
+	options.CopyTo(p, close_fds);
 
 	if (p.cgroup != nullptr && p.cgroup->IsDefined() &&
 	    p.cgroup_session == nullptr) {
@@ -360,6 +359,16 @@ Connection::PrepareChildProcess(PreparedChildProcess &p,
 		p.strings.emplace_front(fmt::format("session-{}", ++session_id_counter));
 		p.cgroup_session = p.strings.front().c_str();
 	}
+}
+
+void
+Connection::PrepareChildProcess(PreparedChildProcess &p,
+				FdHolder &close_fds,
+				const ExecuteOptions &options) noexcept
+{
+	PrepareChildProcess(p, close_fds, options.child_options);
+
+	p.exec_path = options.execute;
 }
 
 bool

src/Connection.hxx
@@ -207,6 +207,10 @@ public:
 
 	static void PrepareChildProcess(PreparedChildProcess &p,
 					FdHolder &close_fds,
+					const ChildOptions &options) noexcept;
+
+	static void PrepareChildProcess(PreparedChildProcess &p,
+					FdHolder &close_fds,
 					const ExecuteOptions &options) noexcept;
 
 	[[gnu::pure]]

commit 8ec5ebe23c0c533b8c3d778cee7f0b47330b7227

SessionChannel: fix ExecRsync() regression
From commit 02019312ce47f66a9eec9c3748deb760903fa4eb

src/SessionChannel.cxx
@@ -396,7 +396,7 @@ SessionChannel::ExecRsync(const char *cmd, const ExecuteOptions &execute_options
 	FdHolder close_fds;
 	PreparedChildProcess p;
 
-	c.PrepareChildProcess(p, close_fds, execute_options.child_options);
+	c.PrepareChildProcess(p, close_fds, execute_options);
 	PreparePipes(p, close_fds);
 	PrepareHome(alloc, p);
 