CM4all/lukko

commit b183632c7c8db643a9951c8e907d04b43a1073aa

ssh/CSupport: add factory method OnChannelCancel()
Provide a hook that allows destructing all factories when the
connection is being closed.

src/ssh/CSupport.cxx
@@ -62,6 +62,11 @@ public:
 		cancel_ptr = *this;
 	}
 
+	~RequestedChannel() noexcept override {
+		if (!canceled)
+			factory.OnChannelCancel();
+	}
+
 	std::unique_ptr<Channel> OnChannelOpenConfirmation(const ChannelOpenConfirmation &p) {
 		assert(p.local_channel == GetLocalChannel());
 

src/AgentForward.cxx
@@ -54,6 +54,7 @@ public:
 	std::unique_ptr<SSH::Channel> CreateChannel(SSH::ChannelInit init) override;
 	void OnChannelOpenFailure(SSH::ChannelOpenFailureReasonCode code,
 				  std::string_view description) noexcept override;
+	void OnChannelCancel() noexcept override;
 
 private:
 	void OnSocketReady(unsigned) noexcept {
@@ -90,6 +91,15 @@ AgentForward::Factory::OnChannelOpenFailure(SSH::ChannelOpenFailureReasonCode co
 				   description, static_cast<unsigned>(code));
 
 	(void)code;
+	delete this;
+}
+
+void
+AgentForward::Factory::OnChannelCancel() noexcept
+{
+	assert(cancel_ptr);
+	cancel_ptr = {};
+
 	delete this;
 }
 

src/SocketForwardListener.cxx
@@ -64,6 +64,7 @@ public:
 	std::unique_ptr<SSH::Channel> CreateChannel(SSH::ChannelInit init) override;
 	void OnChannelOpenFailure(SSH::ChannelOpenFailureReasonCode code,
 				  std::string_view description) noexcept override;
+	void OnChannelCancel() noexcept override;
 
 private:
 	void OnSocketReady(unsigned) noexcept {
@@ -103,6 +104,15 @@ SocketForwardListener::Factory::OnChannelOpenFailure(SSH::ChannelOpenFailureReas
 				   description, static_cast<unsigned>(code));
 
 	(void)code;
+	delete this;
+}
+
+void
+SocketForwardListener::Factory::OnChannelCancel() noexcept
+{
+	assert(cancel_ptr);
+	cancel_ptr = {};
+
 	delete this;
 }
 

src/ssh/CFactory.hxx
@@ -45,6 +45,16 @@ public:
 	 */
 	virtual void OnChannelOpenFailure(ChannelOpenFailureReasonCode code,
 					  std::string_view description) noexcept = 0;
+
+	/**
+	 * Creating the channel has been canceled by the
+	 * #ChannelSupport object, e.g. because the SSH connection is
+	 * being closed.
+	 *
+	 * After returning, the #ChannelFactory is no longer used by
+	 * the #ChannelSupport object.
+	 */
+	virtual void OnChannelCancel() noexcept = 0;
 };
 
 } // namespace SSH

commit 455255099a466b57dded8c5e1dcd13625974b5fe

ssh/CSupport: set `canceled` before deleting this
Prevent the destructor from invoking OnChannelCancel().  Gixes
regression by commit b183632c7c8db643a9951c8e907d04b43a1073aa

src/ssh/CSupport.cxx
@@ -76,6 +76,7 @@ public:
 		}
 
 		auto &_factory = factory;
+		canceled = true;
 		delete this;
 		return _factory.CreateChannel({
 				.local_channel = p.local_channel,