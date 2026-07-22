62a6e673bb2a1f5c04b793fa860a8268d56ffe65
Max Kellermann <max.kellermann@ionos.com>
2026-07-23 01:05:39 +0200

    increment version number to 0.58

--- doc/conf.py
+++ doc/conf.py
@@ -57,7 +57,7 @@ author = 'Max Kellermann'
 # built documents.
 #
 # The short X.Y version.
-version = '0.57'
+version = '0.58'
 # The full version, including alpha/beta/rc tags.
 release = version
 
