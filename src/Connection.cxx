CM4all/lukko

commit 5d91dbd14b99a8a1d11224fa734952717b195ea7

Connection: move code to new class SSH::UserAuthServer

src/Connection.cxx
@@ -752,19 +752,10 @@ Connection::CreateChannel(std::string_view channel_type,
 		};
 }
 
-inline void
-Connection::HandleServiceRequest(std::span<const std::byte> payload)
+void
+Connection::OnUnsupportedService() noexcept
 {
-	const auto p = SSH::ParseServiceRequest(payload);
-
-	if (p.service_name == "ssh-userauth"sv) {
-		have_service_userauth = true;
-		SendPacket(SSH::MakeServiceAccept(p.service_name));
-	} else {
-		++instance.counters.n_unsupported_service;
-		throw Disconnect{SSH::DisconnectReasonCode::SERVICE_NOT_AVAILABLE,
-			"Unsupported service"sv};
-	}
+	++instance.counters.n_unsupported_service;
 }
 
 static bool
@@ -775,11 +766,18 @@ IsValidUsername(std::string_view username) noexcept
 	});
 }
 
-inline Co::EagerInvokeTask
-Connection::CoHandleUserauthRequest(AllocatedArray<std::byte> payload)
+Co::EagerInvokeTask
+Connection::OnUserAuthRequest(AllocatedArray<std::byte> payload)
 {
 	assert(!IsAuthenticated());
 	assert(!channels);
+
+	if (!got_userauth_request) {
+		/* this is the first USERAUTH_REQUEST - reschedule the
+		   timeout with a longer duration */
+		got_userauth_request = true;
+		auth_timeout.Schedule(std::chrono::minutes{2});
+	}
 
 	/* this object aims to prevent timing-based guesses by
 	   delaying all error responses until 100ms have elapsed since
@@ -1145,58 +1143,11 @@ Connection::CoHandleUserauthRequest(AllocatedArray<std::byte> payload)
 	}
 }
 
-inline void
-Connection::OnUserauthCompletion(std::exception_ptr &&error) noexcept
+void
+Connection::OnUserAuthCompletion() noexcept
 {
-	if (error) {
-		try {
-			std::rethrow_exception(std::move(error));
-		} catch (const Disconnect &d) {
-			DoDisconnect(d.reason_code, d.msg);
-		} catch (...) {
-			CloseError(std::move(error));
-		}
-	}
-}
-
-inline void
-Connection::HandleUserauthRequest(std::span<const std::byte> payload)
-{
-	assert(!occupied_task);
-
-	if (IsAuthenticated())
-		/* RFC 4252 section 5.1: "When
-		   SSH_MSG_USERAUTH_SUCCESS has been sent, any further
-		   authentication requests received after that SHOULD
-		   be silently ignored" */
-		return;
-
-	if (!have_service_userauth) {
-		throw Disconnect{
-			SSH::DisconnectReasonCode::PROTOCOL_ERROR,
-			"Service ssh-userauth not requested"sv
-		};
-	}
-
-	if (!got_userauth_request) {
-		/* this is the first USERAUTH_REQUEST - reschedule the
-		   timeout with a longer duration */
-		got_userauth_request = true;
-		auth_timeout.Schedule(std::chrono::minutes{2});
-	}
-
-	/* the payload is owned by the caller, therefore we need to
-	   duplicate it into an AllocatedArray owned by the coroutine,
-	   so the coroutine can keep using it after this method
-	   returns */
-	occupied_task = CoHandleUserauthRequest(AllocatedArray{payload});
-
-	/* we're using EagerInvokeTask here because early errors get
-	   rethrown out of this method instead of being passed to
-	   OnUserauthCompletion(); the latter would destroy this
-	   Connection instance, but this method wouldn't know and
-	   would continue accessing it */
-	occupied_task.Start(BIND_THIS_METHOD(OnUserauthCompletion));
+	assert(user_auth);
+	user_auth.reset();
 }
 
 Co::EagerTask<bool>
@@ -1251,56 +1202,8 @@ Connection::OnEncrypted()
 {
 	SSH::Connection::OnEncrypted();
 
-	AddHandler(*this);
-}
-
-/**
- * Is this message allowed while the connection is "occupied"?
- */
-static constexpr bool
-IsAllowedWhileOccupied(SSH::MessageNumber msg) noexcept
-{
-	switch (msg) {
-	case SSH::MessageNumber::DISCONNECT:
-	case SSH::MessageNumber::IGNORE:
-	case SSH::MessageNumber::NEWCOMPRESS:
-	case SSH::MessageNumber::KEXINIT:
-	case SSH::MessageNumber::NEWKEYS:
-	case SSH::MessageNumber::ECDH_KEX_INIT:
-	case SSH::MessageNumber::ECDH_KEX_INIT_REPLY:
-		return true;
-
-	default:
-		break;
-	}
-
-	return false;
-}
-
-bool
-Connection::HandlePacket(SSH::MessageNumber msg,
-			 std::span<const std::byte> payload)
-{
-	assert(IsEncrypted());
-
-	if (IsOccupied() && !IsAllowedWhileOccupied(msg))
-		throw Disconnect{
-			SSH::DisconnectReasonCode::PROTOCOL_ERROR,
-			"Occupied"sv
-		};
-
-	switch (msg) {
-	case SSH::MessageNumber::SERVICE_REQUEST:
-		HandleServiceRequest(payload);
-		return true;
-
-	case SSH::MessageNumber::USERAUTH_REQUEST:
-		HandleUserauthRequest(payload);
-		return true;
-
-	default:
-		return false;
-	}
+	SSH::UserAuthServerHandler &user_auth_server_handler = *this;
+	user_auth = std::make_unique<SSH::UserAuthServer>(*this, user_auth_server_handler);
 }
 
 void
@@ -1313,7 +1216,7 @@ Connection::OnDisconnecting(SSH::DisconnectReasonCode reason_code,
            postponed */
 	auth_timeout.Cancel();
 	socket_forward_listeners.clear_and_dispose(DeleteDisposer{});
-	occupied_task = {};
+	user_auth.reset();
 
 	if (log_disconnect) {
 		log_disconnect = false;

src/Connection.hxx
@@ -10,6 +10,7 @@
 #include "ssh/GSupport.hxx"
 #include "ssh/CSupport.hxx"
 #include "ssh/SimpleHostKeyChooser.hxx"
+#include "ssh/UserAuthServer.hxx"
 #include "key/Options.hxx"
 #include "event/CoarseTimerEvent.hxx"
 #include "net/AllocatedSocketAddress.hxx"
@@ -45,9 +46,9 @@ class PacketSerializer;
 class Connection final
 	: public AutoUnlinkIntrusiveListHook,
 	  public SSH::Connection,
-	  SSH::ConnectionHandler,
 	  public SSH::GlobalRequestHandler,
 	  public SSH::ChannelHandler,
+	  SSH::UserAuthServerHandler,
 	  OutgoingConnectionHandler
 {
 	Instance &instance;
@@ -62,6 +63,7 @@ class Connection final
 	std::string peer_host;
 #endif
 
+	std::unique_ptr<SSH::UserAuthServer> user_auth;
 	std::unique_ptr<SSH::GlobalRequestSupport> global_requests;
 	std::unique_ptr<SSH::ChannelSupport> channels;
 
@@ -94,14 +96,6 @@ class Connection final
 
 	IntrusiveList<SocketForwardListener> socket_forward_listeners;
 
-	/**
-	 * If this is set, then the connection is currently occupied
-	 * with an asynchronous operation (e.g. lookup in the user
-	 * database).  Until it finishes, most incoming packets will
-	 * cause the connection to be closed.
-	 */
-	Co::EagerInvokeTask occupied_task;
-
 	std::unique_ptr<OutgoingConnection> outgoing;
 
 	struct ProxyHandlers;
@@ -118,8 +112,6 @@ class Connection final
 #endif
 
 	bool log_disconnect = true;
-
-	bool have_service_userauth = false;
 
 	/**
 	 * Tracks whether USERAUTH_REQUEST has been received already.
@@ -324,10 +316,6 @@ public:
 	using SSH::Connection::DoDisconnect;
 
 private:
-	bool IsOccupied() const noexcept {
-		return occupied_task;
-	}
-
 	[[gnu::pure]]
 	const char *GetHome() const noexcept;
 	UniqueFileDescriptor OpenHome() const noexcept;
@@ -369,11 +357,7 @@ private:
 	 */
 	bool IsAcceptedHostPublicKey(std::span<const std::byte> public_key_blob) noexcept;
 
-	void HandleServiceRequest(std::span<const std::byte> payload);
-
 	Co::EagerInvokeTask CoHandleUserauthRequest(AllocatedArray<std::byte> payload);
-	void OnUserauthCompletion(std::exception_ptr &&error) noexcept;
-	void HandleUserauthRequest(std::span<const std::byte> payload);
 
 	void HandleChannelOpen(std::span<const std::byte> payload);
 
@@ -396,16 +380,16 @@ private:
 	void OnDisconnected(SSH::DisconnectReasonCode reason_code,
 			    std::string_view msg) noexcept override;
 
-	/* virtual methods from class SSH::ConnectionHandler */
-	bool HandlePacket(SSH::MessageNumber msg,
-			  std::span<const std::byte> payload) override;
-	using SSH::ConnectionHandler::OnDisconnecting; // suppress -Woverloaded-virtual
-
 	/* virtual methods from class BufferedSocketHandler */
 	void OnBufferedError(std::exception_ptr e) noexcept override;
 
 	/* virtual methods from class SSH::ConnectionDisposer */
 	void Dispose(SSH::Connection *connection) noexcept override;
+
+	/* virtual methods from class SSH::UserAuthServerHandler */
+	void OnUnsupportedService() noexcept override;
+	Co::EagerInvokeTask OnUserAuthRequest(AllocatedArray<std::byte> payload) override;
+	void OnUserAuthCompletion() noexcept override;
 
 	/* virtual methods from class OutgoingConnectionHandler */
 	void OnOutgoingError(std::exception_ptr &&error) noexcept override;

src/ssh/UserAuthServer.cxx
@@ -1,0 +1,144 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <max.kellermann@ionos.com>
+
+#include "UserAuthServer.hxx"
+#include "Connection.hxx"
+#include "MakePacket.hxx"
+#include "PacketSerializer.hxx"
+#include "ParsePacket.hxx"
+#include "util/AllocatedArray.hxx"
+
+using std::string_view_literals::operator""sv;
+
+namespace SSH {
+
+UserAuthServer::UserAuthServer(Connection &_connection,
+			       UserAuthServerHandler &_handler) noexcept
+	:connection(_connection),
+	 handler(_handler)
+{
+	assert(connection.IsEncrypted());
+
+	connection.AddHandler(*this);
+}
+
+UserAuthServer::~UserAuthServer() noexcept = default;
+
+inline void
+UserAuthServer::HandleServiceRequest(std::span<const std::byte> payload)
+{
+	const auto p = ParseServiceRequest(payload);
+
+	if (p.service_name == "ssh-userauth"sv) {
+		have_service_userauth = true;
+		connection.SendPacket(MakeServiceAccept(p.service_name));
+	} else {
+		handler.OnUnsupportedService();
+		throw Connection::Disconnect{
+			DisconnectReasonCode::SERVICE_NOT_AVAILABLE,
+			"Unsupported service"sv,
+		};
+	}
+}
+
+inline void
+UserAuthServer::OnUserAuthCompletion(std::exception_ptr &&error) noexcept
+{
+	if (error) {
+		try {
+			std::rethrow_exception(std::move(error));
+		} catch (const Connection::Disconnect &d) {
+			connection.DoDisconnect(d.reason_code, d.msg);
+		} catch (...) {
+			connection.CloseError(std::move(error));
+		}
+	} else if (connection.IsAuthenticated()) {
+		handler.OnUserAuthCompletion();
+	}
+}
+
+inline void
+UserAuthServer::HandleUserauthRequest(std::span<const std::byte> payload)
+{
+	assert(!occupied_task);
+
+	if (connection.IsAuthenticated())
+		/* RFC 4252 section 5.1: "When
+		   SSH_MSG_USERAUTH_SUCCESS has been sent, any further
+		   authentication requests received after that SHOULD
+		   be silently ignored" */
+		return;
+
+	if (!have_service_userauth) {
+		throw Connection::Disconnect{
+			DisconnectReasonCode::PROTOCOL_ERROR,
+			"Service ssh-userauth not requested"sv
+		};
+	}
+
+	/* the payload is owned by the caller, therefore we need to
+	   duplicate it into an AllocatedArray owned by the coroutine,
+	   so the coroutine can keep using it after this method
+	   returns */
+	occupied_task = handler.OnUserAuthRequest(AllocatedArray{payload});
+
+	/* we're using EagerInvokeTask here because early errors get
+	   rethrown out of this method instead of being passed to
+	   OnUserAuthCompletion(); the latter would destroy this
+	   Connection instance, but this method wouldn't know and
+	   would continue accessing it */
+	occupied_task.Start(BIND_THIS_METHOD(OnUserAuthCompletion));
+}
+
+/**
+ * Is this message allowed while the connection is "occupied"?
+ */
+static constexpr bool
+IsAllowedWhileOccupied(MessageNumber msg) noexcept
+{
+	using enum MessageNumber;
+	switch (msg) {
+	case DISCONNECT:
+	case IGNORE:
+	case NEWCOMPRESS:
+	case KEXINIT:
+	case NEWKEYS:
+	case ECDH_KEX_INIT:
+	case ECDH_KEX_INIT_REPLY:
+		return true;
+
+	default:
+		break;
+	}
+
+	return false;
+}
+
+bool
+UserAuthServer::HandlePacket(MessageNumber msg,
+			     std::span<const std::byte> payload)
+{
+	assert(connection.IsEncrypted());
+
+	if (IsOccupied() && !IsAllowedWhileOccupied(msg))
+		throw Connection::Disconnect{
+			DisconnectReasonCode::PROTOCOL_ERROR,
+			"Occupied"sv
+		};
+
+	switch (msg) {
+	case MessageNumber::SERVICE_REQUEST:
+		HandleServiceRequest(payload);
+		return true;
+
+	case MessageNumber::USERAUTH_REQUEST:
+		HandleUserauthRequest(payload);
+		return true;
+
+	default:
+		return false;
+	}
+}
+
+} // namespace SSH

src/ssh/UserAuthServer.hxx
@@ -1,0 +1,63 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <max.kellermann@ionos.com>
+
+#pragma once
+
+#include "Handler.hxx"
+#include "co/InvokeTask.hxx"
+
+#include <exception>
+
+template<typename T> class AllocatedArray;
+
+namespace SSH {
+
+class Connection;
+
+class UserAuthServerHandler {
+public:
+	virtual void OnUnsupportedService() noexcept {}
+
+	[[nodiscard]]
+	virtual Co::EagerInvokeTask OnUserAuthRequest(AllocatedArray<std::byte> payload) = 0;
+
+	virtual void OnUserAuthCompletion() noexcept {}
+};
+
+class UserAuthServer final : ConnectionHandler {
+	Connection &connection;
+
+	UserAuthServerHandler &handler;
+
+	/**
+	 * If this is set, then the connection is currently occupied
+	 * with an asynchronous operation (e.g. lookup in the user
+	 * database).  Until it finishes, most incoming packets will
+	 * cause the connection to be closed.
+	 */
+	Co::EagerInvokeTask occupied_task;
+
+	bool have_service_userauth = false;
+
+public:
+	UserAuthServer(Connection &_connection,
+		       UserAuthServerHandler &_handler) noexcept;
+	~UserAuthServer() noexcept;
+
+private:
+	bool IsOccupied() const noexcept {
+		return occupied_task;
+	}
+
+	void HandleServiceRequest(std::span<const std::byte> payload);
+	void OnUserAuthCompletion(std::exception_ptr &&error) noexcept;
+	void HandleUserauthRequest(std::span<const std::byte> payload);
+
+protected:
+	/* virtual methods from class ConnectionHandler */
+	bool HandlePacket(MessageNumber msg,
+			  std::span<const std::byte> payload) override;
+};
+
+} // namespace SSH

src/ssh/meson.build
@@ -28,6 +28,7 @@ libssh = static_library(
   'TerminalMode.cxx',
   'EarlyDisconnect.cxx',
   'UserAuthClient.cxx',
+  'UserAuthServer.cxx',
   include_directories: inc,
   dependencies: [
     cipher_dep,

commit b320b4a8f86ade626a96ed8b890c733d276813d7

Connection: call SetAuthenticated() even when proxying
The connection to our client is indeed authenticated, and postpoing
that call to OnOutgoingUserauthSuccess() was not helpful for anything.
On the other hand, commit 5d91dbd14b99a8a1d11224fa734952717b195ea7
caused a regression because while we were connecting to the final
server, UserAuthServer::OnUserAuthCompletion() would see that
IsAuthenticated()==false and thus never fire
handler.OnUserAuthCompletion(), leaving the UserAuthServer object
alive, allowing for recursive auths.

src/Connection.cxx
@@ -1123,6 +1123,8 @@ Connection::OnUserAuthRequest(AllocatedArray<std::byte> payload)
 
 	auth_timeout.Cancel();
 
+	SetAuthenticated();
+
 	if (const auto proxy_to = listener.GetProxyTo(arch, sticky_source);
 	    !proxy_to.IsNull()) {
 		auto s = co_await CoConnectSocket(GetEventLoop(), proxy_to, std::chrono::seconds{10});
@@ -1134,8 +1136,10 @@ Connection::OnUserAuthRequest(AllocatedArray<std::byte> payload)
 								listener.GetProxyHostKeys(),
 								std::move(s), handler);
 		outgoing_ready = false;
+
+		/* USERAUTH_SUCCESS will be sent later by
+		   OnOutgoingUserauthSuccess() */
 	} else {
-		SetAuthenticated();
 		SendPacket(SSH::PacketSerializer{SSH::MessageNumber::USERAUTH_SUCCESS});
 
 		global_requests = std::make_unique<SSH::GlobalRequestSupport>(*this, *this);
@@ -1344,7 +1348,6 @@ Connection::OnOutgoingUserauthSuccess()
 
 	proxy_handlers = std::make_unique<ProxyHandlers>(*this, *outgoing);
 
-	SetAuthenticated();
 	SendPacket(SSH::PacketSerializer{SSH::MessageNumber::USERAUTH_SUCCESS});
 }
 