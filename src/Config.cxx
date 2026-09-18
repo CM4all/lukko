4c75c4c608f0cb0d44a3a1d1eab1baff65745c6f
Max Kellermann <max.kellermann@ionos.com>
2026-09-18 08:50:46 +0200

    Config: disable allow_any_uid_gid
    This kludge was never necessary.

--- src/Config.cxx
+++ src/Config.cxx
@@ -63,9 +63,6 @@ Config::Config()
 	   it breaks PID_NAMESPACE_NAME */
 	spawn.pid_namespace = false;
 
-	// TODO implement SpawnConfig properly
-	spawn.allow_any_uid_gid = true;
-
 #ifdef HAVE_LIBSYSTEMD
 	spawn.systemd_scope = "lukko-spawn.scope";
 	spawn.systemd_scope_description = "The Lukko child process spawner";
