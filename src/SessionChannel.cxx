CM4all/lukko

commit 3b25509b2f73570390bcacff4173df8005cf2bda

SocketChannel: close channel on send error
Throwing an exception will cause the whole SSH connection to be
closed, but only this channel should really be closed.  Most of the
time, this problem did not occur because EPOLLHUP was usually handled
earlier.

src/SocketChannel.cxx
@@ -62,7 +62,9 @@ SocketChannel::OnBufferedData(std::span<const std::byte> payload)
 			return 0;
 		}
 
-		throw MakeSocketError(e, "Failed to send");
+		// TODO log error?
+		Close();
+		return 0;
 	}
 
 

commit cb28ea903b505a9b049ea9bd6b737987603eb15f

SocketChannel: return CLOSED after channel was closed
Fixes a use-after-free regression by commit 3b25509b2

src/SessionChannel.cxx
@@ -990,7 +990,8 @@ try {
 
 	if (events & PipeEvent::WRITE) {
 		tty.CancelOnlyWrite();
-		ReadBuffer();
+		if (!ReadBuffer()) [[unlikely]]
+			return;
 	}
 
 	if ((events & PipeEvent::READ) == 0)

src/SocketChannel.cxx
@@ -64,7 +64,7 @@ SocketChannel::OnBufferedData(std::span<const std::byte> payload)
 
 		// TODO log error?
 		Close();
-		return 0;
+		return CLOSED;
 	}
 
 
@@ -106,7 +106,8 @@ try {
 
 	if (events & SocketEvent::WRITE) {
 		socket.CancelOnlyWrite();
-		ReadBuffer();
+		if (!ReadBuffer()) [[unlikely]]
+			return;
 	}
 
 	/* is the kernel's receive buffer empty? */

src/ssh/BufferedChannel.cxx
@@ -19,6 +19,9 @@ BufferedChannel::OnData(std::span<const std::byte> payload)
 	}
 
 	const auto nbytes = OnBufferedData(payload);
+	if (nbytes == CLOSED) [[unlikely]]
+		return;
+
 	if (nbytes < payload.size()) {
 		const auto rest = payload.subspan(nbytes);
 		queue.Push(rest);
@@ -41,7 +44,7 @@ BufferedChannel::OnEof()
 		eof_pending = true;
 }
 
-void
+bool
 BufferedChannel::ReadBuffer()
 {
 	assert(queue.empty() == (queue_bytes == 0));
@@ -53,6 +56,9 @@ BufferedChannel::ReadBuffer()
 		assert(payload.size() <= queue_bytes);
 
 		const auto nbytes = OnBufferedData(payload);
+		if (nbytes == CLOSED) [[unlikely]]
+			return false;
+
 		queue.Consume(nbytes);
 		queue_bytes -= nbytes;
 
@@ -62,7 +68,7 @@ BufferedChannel::ReadBuffer()
 		if (nbytes < payload.size()) {
 			if (consumed)
 				MaybeSendWindowAdjust();
-			return;
+			return true;
 		}
 	}
 
@@ -73,6 +79,8 @@ BufferedChannel::ReadBuffer()
 		OnBufferedEof();
 	} else if (consumed)
 		MaybeSendWindowAdjust();
+
+	return true;
 }
 
 } // namespace SSH

src/ssh/BufferedChannel.hxx
@@ -31,6 +31,12 @@ protected:
 	 */
 	std::size_t max_receive_window = 0;
 
+	/**
+	 * Special return value for OnBufferedData() to indicate that
+	 * the method has closed the channel.
+	 */
+	static constexpr std::size_t CLOSED = ~std::size_t{0};
+
 public:
 	using Channel::Channel;
 
@@ -55,15 +61,21 @@ private:
 
 protected:
 	/**
+	 * Continue reading from the buffer, feed into
+	 * OnBufferedData().
 	 *
+	 * @return false if the channel has been closed
 	 */
-	void ReadBuffer();
+	bool ReadBuffer();
 
 	/**
 	 * @return the number of bytes consumed; if this is less than
 	 * the given payload size, then the transmission is paused and
 	 * method is expected to call ReadBuffer() eventually to
 	 * resume the transmission
+	 *
+	 * The special return value #CLOSED indicates that this method
+	 * has closed the channel.
 	 */
 	[[nodiscard]]
 	virtual std::size_t OnBufferedData(std::span<const std::byte> payload) = 0;